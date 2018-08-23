998503fe653b4afd559965c7a9aaa3a216dd1715
GlennJenkins <glenn.jenkins@digitickets.co.uk>
2018-08-23 14:40:01 +0100

    Add mandatory method to the gateway.

--- src/SimpleInterfaceGateway.php
+++ src/SimpleInterfaceGateway.php
@@ -8,6 +8,16 @@ use Omnipay\Common\AbstractGateway;
 
 class SimpleInterfaceGateway extends AbstractGateway
 {
+    /**
+     * Get gateway display name
+     *
+     * This can be used by carts to get the display name for each gateway.
+     */
+    public function getName()
+    {
+        return 'Pay360 (Simple Interface)';
+    }
+    
     public function purchase(array $parameters = array())
     {
         return $this->createRequest(PurchaseRequest::class, $parameters);
