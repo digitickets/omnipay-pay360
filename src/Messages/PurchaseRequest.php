df1567725e7d8774c1ecb986ed9aa12338fd6f7e
GlennJenkins <glenn.jenkins@digitickets.co.uk>
2018-08-23 15:57:28 +0100

    Tweaks to purchase request; added temporary debugging.

--- src/Messages/PurchaseRequest.php
+++ src/Messages/PurchaseRequest.php
@@ -37,44 +37,62 @@ class PurchaseRequest extends AbstractRequest
         return $this->setParameter('amount', $value);
     }
     
+    protected function generateDigest(
+        \scpService_subject $subject,
+        \scpService_requestIdentification $requestIdentification,
+        \scpService_signature$signature
+    ){
+error_log('generateDigest...');
+        return 'TBC';
+    }
+    
     public function getData()
     {
+error_log('getData...');
         $unknown = 'n/k'; // @TODO: Temporary
         $optional = 'TBA'; // @TODO: Temporary
-        
+        $config = '11223344';
+
         $subject = new \scpService_subject();
         $subject->subjectType = \scpService_subjectType::CAPITA_PORTAL;
-        $subject->identifier = $unknown;
+        $subject->identifier = $config;  // Same as $routing->siteId
         $subject->systemCode = \scpService_systemCode::SCP;
-        
+error_log('After $subject');
+
         $requestIdentification = new \scpService_requestIdentification();
         $requestIdentification->uniqueReference = $this->getTransactionId();
         $requestIdentification->timeStamp = date('YmdHis'); // Format: YYYYMMDDHHMMSS
-        
+error_log('After $requestIdentification');
+
         $signature = new \scpService_signature();
         $signature->algorithm = \scpService_algorithm::ORIGINAL;
-        $signature->hmacKeyID = $unknown;
-        $signature->digest = $unknown;
-    
+        $signature->hmacKeyID = $config;
+        $signature->digest = $this->generateDigest($subject, $requestIdentification, $signature);
+error_log('After $signature');
+
         $credentials = new \scpService_credentials();
         $credentials->subject = $subject;
         $credentials->requestIdentification = $requestIdentification;
         $credentials->signature = $signature;
-    
+error_log('After $credentials');
+
         $routing = new \scpService_routing();
         $routing->returnUrl = $this->getReturnUrl();
         $routing->backUrl = $this->getBackUrl();
-        $routing->siteId = $unknown;
-        $routing->scpId = $unknown;
-    
+        $routing->siteId = $config;
+        $routing->scpId = $config;
+error_log('After $routing');
+
         $saleSummary = new \scpService_summaryData();
         $saleSummary->description = 'Online Sale'; // Gets overwritten by items, apparently.
         $saleSummary->amountInMinorUnits = 100 * $this->getAmount();
         $saleSummary->reference = 'Reference'; // Gets overwritten by items, apparently.
         $saleSummary->displayableReference = 'Displayable Reference'; // Gets overwritten by items, apparently.
-    
+error_log('After $saleSummary');
+
         $items = $unknown; // @TODO: Comes from the item bag thing.
-        
+error_log('After $items');
+
         $sale = new \scpService_simpleSale();
         $sale->saleSummary = $saleSummary;
         $sale->deliveryDetails = $optional;
@@ -82,19 +100,22 @@ class PurchaseRequest extends AbstractRequest
         $sale->postageAndPacking = $optional;
         $sale->surchargeable = $optional;
         $sale->items = $items;
-    
+error_log('After $sale');
+
         $data = new \scpService_scpSimpleInvokeRequest();
         $data->credentials = $credentials;
         $data->requestType = \scpService_requestType::PAY_ONLY;
-        $data->requestId = $unknown;
+        $data->requestId = $unknown; // Customer-supplied request id - TBC
         $data->routing = $routing;
         $data->sale = $sale;
-        
+error_log('After $data');
+
         return $data;
     }
     
     public function sendData($data)
     {
+error_log('sendData...');
         return $this->response = new PurchaseResponse($this, $data);
     }
 }
